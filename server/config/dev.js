5152996dd1bf312ddc848a2106726703adfcf1b0
Vivek23699 <vivek23699@gmail.com>
2024-01-04 23:04:31 +0530

    updated package

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,7 @@
+
 module.exports={
-    MONGOURI:"mongodb+srv://hirevivek12:Vivek123@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: "qazxswedcvfrtgbnhy",
-    SENDGRID_API:"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",
-    EMAIL:"http:localhost:3000"
+    MONGOURI:process.env.MONGOURI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
 }
