00f88957cedfa5edf69f5039b7b4643dbeff00ad
Vivek23699 <vivek23699@gmail.com>
2024-01-13 21:49:37 +0530

    final update for proxy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://instagram-clone-djek.onrender.com",
+  // baseURL: "https://instagram-clone-djek.onrender.com",
   // baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
